bec73841ff34baa0ef2340812b6248fc21a33d2c
Jane Doe <radoslava.kacova@mou.cz>
2026-08-28 16:18:33 +0200

    final revision part 1
    final revision of the first 4 modules + path correction in main.java - it didn't generate markdown file from yaml at right place

--- src/main/java/org/fairgenomes/generator/Main.java
+++ src/main/java/org/fairgenomes/generator/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String args[]) throws Exception {
         System.out.println("Starting...");
         long start = System.nanoTime();
-        new GenerateOutputs("fair-genomes.yml").generateResources();
+        new GenerateOutputs("./fair-genomes.yml").generateResources();
         //new GenerateOutputs("extensions/palga-protocol-molecular-testing.yml").generateResources();
         System.out.println("Done! Completed in " + ((System.nanoTime()-start)/1000000)+"ms.");
     }
